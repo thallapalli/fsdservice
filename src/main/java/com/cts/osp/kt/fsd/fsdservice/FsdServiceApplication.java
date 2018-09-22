bbf82cd161422fda5b976d314cf49ba822ce0441
thallapalli <thallapallikarnakar@gmail.com>
2018-09-22 14:51:47 -0500

    Inital commit

---
+++ src/main/java/com/cts/osp/kt/fsd/fsdservice/FsdServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cts.osp.kt.fsd.fsdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdServiceApplication.class, args);
+	}
+}
